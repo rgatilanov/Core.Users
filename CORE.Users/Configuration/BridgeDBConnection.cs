46e54dae58648a40650ef2c71b3d6cfab1dd89b6
Gerardo <Villalobos>
2021-10-20 13:47:53 -0500

    Deploy en IIS (Se comenta decrypt de cadena de conexión)

--- CORE.Users/Configuration/BridgeDBConnection.cs
+++ CORE.Users/Configuration/BridgeDBConnection.cs
@@ -14,7 +14,8 @@ namespace CORE.Users.Configuration
     {
         public static IConnectionDB<T> Create(string ConnectionString, DbEnum DB)
         {
-            return Factorizer<T>.Create(EncryptTool.Decrypt(ConnectionString), DB);
+            //return Factorizer<T>.Create(EncryptTool.Decrypt(ConnectionString), DB);
+            return Factorizer<T>.Create(ConnectionString, DB);
         }
     }
 }
--- CORE.Users/Configuration/ConnectionStrings.cs
+++ CORE.Users/Configuration/ConnectionStrings.cs
@@ -6,7 +6,8 @@ namespace CORE.Users.Configuration
 {
     public static class ConnectionStrings
     {
-        public static string LocalServer = "ZKvO+FHjoZKEEE76jdbyAnEqZ+A9ffaZ6Z02Yf8iN0BJo1Dk4I2CysY6Ho+5PPjZA/Biz0LT9QT0yjeF7o5GAhw6mWdUs+9tAmXgM4tuhBs/Lc7N7zJoRhXkvXLO2zv8NIN7q4sdBKd3MboQ4rE3zXc6h1eJG5fjgBt7OIBIagCXdUdM4l88gVfH3noSosIw";
+        //public static string LocalServer = "ZKvO+FHjoZKEEE76jdbyAnEqZ+A9ffaZ6Z02Yf8iN0BJo1Dk4I2CysY6Ho+5PPjZA/Biz0LT9QT0yjeF7o5GAhw6mWdUs+9tAmXgM4tuhBs/Lc7N7zJoRhXkvXLO2zv8NIN7q4sdBKd3MboQ4rE3zXc6h1eJG5fjgBt7OIBIagCXdUdM4l88gVfH3noSosIw";
+        public static string LocalServer = "Data Source=localhost;Database=CORE_3CRUD;user id=covid;password=123123";
 
         public static string CloudServer = "";
 
